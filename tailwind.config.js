a26d7bf5e5935d0d9769794b685e945cf98b839d
Cem <bcemcankaya@gmail.com>
2024-09-20 13:46:35 +0300

    Refactor Tailwind Config File: add custom box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily:{
-        poppins: ["Poppins", "sans-serif"]
-      }
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      boxShadow: {
+        "box-shadow":
+          "rgba(255, 255, 255, 0.2) 0px 0px 15px, rgba(255, 255, 255, 0.15) 0px 0px 3px 1px",
+      },
     },
   },
   plugins: [],
