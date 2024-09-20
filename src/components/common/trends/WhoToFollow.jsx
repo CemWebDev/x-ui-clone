c02e5e621bfd32f802725d46da1d42c620df8d20
Cem <bcemcankaya@gmail.com>
2024-09-20 13:53:30 +0300

    feat: add WhoToFollow component
    - map SuggestedFollow components
    - add show more NavLink

--- src/components/common/trends/WhoToFollow.jsx
+++ src/components/common/trends/WhoToFollow.jsx
@@ -1,5 +1,19 @@
+import { NavLink } from "react-router-dom";
+import SuggestedFollow from "./SuggestedFollow";
+import { whoToFollow } from "../../../constant";
+
 const WhoToFollow = () => {
-  return <div>WhoToFollow</div>;
+  return (
+    <div className="w-full border border-neutral-800/70 pt-4 rounded-2xl mt-4 overflow-hidden">
+      <h1 className="pl-4  mb-1.5 font-semibold text-lg">Who To Follow</h1>
+      {whoToFollow.map((user) => (
+        <SuggestedFollow key={user.id} user={user} /> 
+      ))}
+      <NavLink className="text-blue-500 block px-4 mt-2 text-sm py-2.5 hover:bg-neutral-800/70 text-start">
+        Show more
+      </NavLink>
+    </div>
+  );
 };
 
 export default WhoToFollow;
