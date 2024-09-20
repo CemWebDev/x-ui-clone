f9f8efcfb2c8aead80c5eb2aa0a9f10f94979a84
Cem <bcemcankaya@gmail.com>
2024-09-20 21:57:29 +0300

    fix: sidebar link (more) popover z-index bug

--- src/components/common/sidebar/Sidebar.jsx
+++ src/components/common/sidebar/Sidebar.jsx
@@ -8,7 +8,7 @@ import { FaFeather } from "react-icons/fa";
 
 const Sidebar = () => {
   return (
-    <aside className="min-h-screen sticky top-0 max-h-screen flex flex-col w-20 xl:w-64 pl-3 pr-4">
+    <aside className="min-h-screen sticky top-0 max-h-screen flex flex-col w-20 xl:w-64 pl-3 pr-4 z-50">
       <div className="mt-3 text-3xl pl-2.5">
         <NavLink to="/">
           <FaXTwitter />
