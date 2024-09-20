9272b7c981c5edde6099a9531c56054a32bf287f
Cem <bcemcankaya@gmail.com>
2024-09-20 13:45:12 +0300

    Refactor Button Component: add variant prop, class handling and extend size options

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,30 +1,38 @@
 import PropTypes from "prop-types";
 import classNames from "classnames";
-import { FaFeather } from "react-icons/fa";
 
-const Button = ({ size, children }) => {
-  const buttonClass = classNames({
-    "p-3 h-9": size === "normal",
-    "p-4 h-[52px] w-full xl:w-[90%] w-[52px] xl:w-auto text-lg":
-      size === "large",
-    "bg-blue-500 flex items-center justify-center text-white rounded-full hover:bg-blue-600 transition-colors": true,
-  });
+const Button = ({ size, variant, children, className, ...props }) => {
+  const buttonClass = classNames(
+    {
+      "px-4 h-8 text-sm": size === "small",
+      "px-4 h-9 text-sm": size === "normal",
+      "p-4 h-14 w-full xl:w-[90%] w-[52px] xl:w-auto text-lg": size === "large",
+      "bg-blue-500 hover:bg-blue-600 text-white": variant === "primary",
+      "text-black bg-white hover:bg-gray-200": variant === "white",
+      "bg-transparent border-gray-200 ": variant === "follow-outline",
+      "flex items-center border border-transparent justify-center rounded-full font-semibold transition-colors": true,
+    },
+    className
+  );
 
   return (
-    <button className={buttonClass}>
-      <FaFeather className="block xl:hidden" />
-      <span className="hidden xl:block">{children}</span>
+    <button className={buttonClass} {...props}>
+      {children}
     </button>
   );
 };
 
 Button.propTypes = {
-  size: PropTypes.oneOf(["normal", "large"]),
+  size: PropTypes.oneOf(["small", "normal", "large"]),
   children: PropTypes.node.isRequired,
+  variant: PropTypes.oneOf(["primary", "white"]),
+  className: PropTypes.string,
 };
 
 Button.defaultProps = {
   size: "normal",
+  variant: "primary",
+  className: "",
 };
 
 export default Button;
