3d1791bccc8b927aa415e8e3bef81a7e37e24e08
Cem <bcemcankaya@gmail.com>
2024-09-20 14:00:31 +0300

    refactor Subscribe component: remove unnecessary box shadow for cleaner UI

--- src/components/common/trends/Subscribe.jsx
+++ src/components/common/trends/Subscribe.jsx
@@ -2,7 +2,7 @@ import Button from "../../UI/Button";
 
 const Subscribe = () => {
   return (
-    <div className="bg-black border border-neutral-800/90 rounded-2xl shadow-sm p-4">
+    <div className="bg-black border border-neutral-800/90 rounded-2xl p-4 w-full">
       <h1 className="text-xl font-semibold">Subscribe to Premium</h1>
       <p className="text-sm my-2">
         Subscribe to unlock new features and if eligible, receive a share of ads
