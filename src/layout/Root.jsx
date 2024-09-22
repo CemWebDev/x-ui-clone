0c629431249ab48a2a07c1a4e175296b9ab182bc
Cem <bcemcankaya@gmail.com>
2024-09-22 15:20:10 +0300

    Feat: Add dynamic appearance handling and conditional TrendsBar visibility in Root layout
    - Integrated useAppearance hook to dynamically update theme colors and font size
    - Applied custom CSS variables for background, text colors, and font size
    - Added conditional rendering of TrendsBar based on route path (hidden for /settings)
    - Refactored Sidebar and TrendsBar imports to use index files

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,15 +1,68 @@
-import { Outlet } from "react-router-dom";
-import Sidebar from "../components/common/sidebar/Sidebar";
-import TrendsBar from "../components/common/trends/TrendsBar";
+import { Outlet, useLocation } from "react-router-dom";
+import Sidebar from "../components/common/sidebar/index";
+import TrendsBar from "../components/common/trends/index";
+import { useEffect } from "react";
+import { useAppearance } from "../store/appearance/hooks";
 
 const Root = () => {
+  const appearance = useAppearance();
+
+  useEffect(() => {
+    document.documentElement.style.setProperty(
+      "--background-primary",
+      appearance.backgroundColor.primary
+    );
+    document.documentElement.style.setProperty(
+      "--background-secondary",
+      appearance.backgroundColor.secondary
+    );
+    document.documentElement.style.setProperty(
+      "--background-third",
+      appearance.backgroundColor.third
+    );
+
+    document.documentElement.style.setProperty(
+      "--color-primary",
+      appearance.color.primary
+    );
+
+    document.documentElement.style.setProperty(
+      "--text-color",
+      appearance.color.textColor
+    );
+
+    document.documentElement.style.setProperty(
+      "--text-color-secondary",
+      appearance.color.textColorSecondary
+    );
+
+    document.documentElement.style.setProperty(
+      "--font-size",
+      appearance.fontSize + "px"
+    );
+
+  }, [appearance]);
+
+  const location = useLocation();
+
+  const hiddenTrendsBar = ["/settings"];
+  const visibleTrendsBar = !hiddenTrendsBar.some((path) =>
+    location.pathname.startsWith(path)
+  );
+
   return (
     <div className="max-w-screen-xl mx-auto flex font-poppins">
       <Sidebar />
-      <main className="flex-1 border-x border-neutral-800/90 mr-7">
-        <Outlet />
-      </main>
-      <TrendsBar />
+      <div className="flex-1 flex">
+        <main className="flex-1 pt-2 border-neutral-500/50 border-x min-w-0">
+          <Outlet />
+        </main>
+        {visibleTrendsBar && (
+          <div className="">
+            <TrendsBar />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
