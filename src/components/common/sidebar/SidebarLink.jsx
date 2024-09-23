6b14f4e83064f6536af78bdba9c61c8206dfb506
Cem <bcemcankaya@gmail.com>
2024-09-23 13:54:47 +0300

    feat: hide specific Sidebar links based on font size for improved responsiveness

--- src/components/common/sidebar/SidebarLink.jsx
+++ src/components/common/sidebar/SidebarLink.jsx
@@ -1,8 +1,14 @@
 import { NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
 import MorePopover from "./MorePopover";
+import { useAppearance } from "../../../store/appearance/hooks";
 
-const SidebarLink = ({ text, icon: Icon, notification, path }) => {
+const SidebarLink = ({ text, icon: Icon, notification, path, id }) => {
+  const { fontSize } = useAppearance();
+
+  const shouldHide = (fontSize >= 16 && id === 6) || (fontSize >= 17 && (id === 7 || id === 8));
+
+
   if (text === "More") {
     return (
       <li className="w-full">
@@ -11,18 +17,18 @@ const SidebarLink = ({ text, icon: Icon, notification, path }) => {
     );
   }
   return (
-    <li className="w-full">
+    <li className={`w-full ${shouldHide ? "hidden" : ""}`}>
       <NavLink className="block group" {...(path ? { to: path } : {})}>
         <div className="inline-flex  items-center gap-4 group-hover-effect rounded-full p-2.5 transition-colors relative">
           <div className="relative">
-            {notification && (  
+            {notification && (
               <div className="absolute w-[18px] h-[18px] -right-1 -top-1.5 bg-[color:var(--color-primary)] text-white flex items-center justify-center text-xs rounded-full">
                 {notification}
               </div>
             )}
             <Icon className="text-2xl" />
           </div>
-          <span className="text-[18px] hidden xl:block">{text}</span>
+          <span className="text-[1.2rem] hidden xl:block">{text}</span>
         </div>
       </NavLink>
     </li>
@@ -33,6 +39,7 @@ SidebarLink.propTypes = {
   text: PropTypes.string.isRequired,
   icon: PropTypes.elementType.isRequired,
   notification: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  id: PropTypes.number.isRequired,
   path: PropTypes.string,
 };
 
