0ff3049e11224c8221ee59be0026bc6136d78a9d
Cem <bcemcankaya@gmail.com>
2024-09-28 12:56:08 +0300

    refactor: remove unnecessary header tag due to HeaderWrapper component

--- src/components/explore/Header.jsx
+++ src/components/explore/Header.jsx
@@ -5,40 +5,38 @@ import TabItem from "../tabs/TabItem";
 const Header = () => {
   const { activeTab, setActiveData } = useTab("explore");
   return (
-    <header className="sticky backdrop-blur-md top-0">
-      <HeaderWrapper>
-        <TabItem
-          isActive={activeTab === "for-you"}
-          onClick={() => setActiveData("for-you")}
-        >
-          For You
-        </TabItem>
-        <TabItem
-          isActive={activeTab === "trending"}
-          onClick={() => setActiveData("trending")}
-        >
-          Trending
-        </TabItem>
-        <TabItem
-          isActive={activeTab === "news"}
-          onClick={() => setActiveData("news")}
-        >
-          News
-        </TabItem>
-        <TabItem
-          isActive={activeTab === "sports"}
-          onClick={() => setActiveData("sports")}
-        >
-          Sports
-        </TabItem>
-        <TabItem
-          isActive={activeTab === "entertainment"}
-          onClick={() => setActiveData("entertainment")}
-        >
-          Entertainment
-        </TabItem>
-      </HeaderWrapper>
-    </header>
+    <HeaderWrapper>
+      <TabItem
+        isActive={activeTab === "for-you"}
+        onClick={() => setActiveData("for-you")}
+      >
+        For You
+      </TabItem>
+      <TabItem
+        isActive={activeTab === "trending"}
+        onClick={() => setActiveData("trending")}
+      >
+        Trending
+      </TabItem>
+      <TabItem
+        isActive={activeTab === "news"}
+        onClick={() => setActiveData("news")}
+      >
+        News
+      </TabItem>
+      <TabItem
+        isActive={activeTab === "sports"}
+        onClick={() => setActiveData("sports")}
+      >
+        Sports
+      </TabItem>
+      <TabItem
+        isActive={activeTab === "entertainment"}
+        onClick={() => setActiveData("entertainment")}
+      >
+        Entertainment
+      </TabItem>
+    </HeaderWrapper>
   );
 };
 
